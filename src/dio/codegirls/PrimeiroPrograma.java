86ab788d0e95024cd3a9e82940842a6cdf000e40
joanaMartinsS <joana.martins0514@gmail.com>
2022-07-12 14:26:42 -0300

    Quarta tentativa de commit

--- src/dio/codegirls/PrimeiroPrograma.java
+++ src/dio/codegirls/PrimeiroPrograma.java
@@ -9,8 +9,8 @@ public class PrimeiroPrograma {
 		Gato gato = new Gato(); // Classe em outro arquivo
 		Livros livros = new Livros(); // Classe no mesmo arquivo 
 		
-		//System.out.println(gato); // Mostra o objeto 
-		//System.out.println(livros); // Mostra o endereço de memória porque não tem o StringOut
+		System.out.println(gato); // Mostra o objeto 
+		System.out.println(livros); // Mostra o endereço de memória porque não tem o StringOut
 		
 		/*int a = 2;
 		int b = 5;
